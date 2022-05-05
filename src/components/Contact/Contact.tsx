540c738f4d821a9502b07effde0e27c495f76439
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-05-05 01:15:58 -0400

    Contact form styling

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import './Contact.scss';
 
 export default function Contact() {
 	const [letterClass, setletterClass] = useState('text-animate');
@@ -13,11 +14,35 @@ export default function Contact() {
 	}, []);
 	return (
 		<>
-			<div className="container contact=page">
+			<div className="container contact-page">
 				<div className="text-zone">
 					<h1>
 						<AnimatedLetters letterClass={letterClass} idx={15} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}></AnimatedLetters>
 					</h1>
+					<p>
+						I am open to any employment opportunities, especially in fintech or web-development. However, if you have any other requests or questions, please don't hesitate to contact me! Submit the form below and I will respond as soon as possible.
+					</p>
+					<div className="contact-form">
+						<form>
+							<ul>
+								<li className="half">
+									<input type="text" name="name" placeholder="Name" required></input>
+								</li>
+								<li className="half">
+									<input type="email" name="email" placeholder="Email" required></input>
+								</li>
+								<li>
+									<input type="text" name="subject" placeholder="Subject" required></input>
+								</li>
+								<li>
+									<textarea name="message" placeholder="Message" required></textarea>
+								</li>
+								<li className="button-align">
+									<input type="submit" className="flat-button" value="SEND" />
+								</li>
+							</ul>
+						</form>
+					</div>
 				</div>
 			</div>
 			<Loader type="ball-scale-ripple"></Loader>
